69607d5626a25edd73c2c5390bddbd85ff3a4cb5
Alexander Williams <richwill7799@gmail.com>
2019-12-06 22:53:44 +0100

    WIP first moving player char

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+    public float zCoord;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = transform.position + Vector3.right * Input.GetAxis("Horizontal") +
+                             Vector3.up * Input.GetAxis("Vertical") + Vector3.up * zCoord;
+        
+    }
+}
