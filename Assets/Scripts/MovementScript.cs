ff21bc2fa7b89e2888365691cd825b566e85a680
Alexander Williams <richwill7799@gmail.com>
2019-12-06 23:53:59 +0100

    moving player char + jump done

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -3,18 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementScript : MonoBehaviour {
-    public float zCoord;
+    [Header("Jump Variables")]
+    public float zCoord; //offset from floor
+    public float startJumpHeight, gravity, cooldown; //start jump boost, downward force, time in sec between jumps
+    [Space(10)]
+    
+    public float speed;
+
+    private float zCoordDelta; //movement up or down
+    private float lastJump;
     
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        zCoord = 0;
         
     }
 
     // Update is called once per frame
     void Update() {
-        transform.position = transform.position + Vector3.right * Input.GetAxis("Horizontal") +
-                             Vector3.up * Input.GetAxis("Vertical") + Vector3.up * zCoord;
         
+        //Jump stuff ####
+        if (zCoord <= 0 && Input.GetAxis("Jump") >= 0.8 && lastJump+cooldown<Time.time) {
+            Jump();
+        }
+        
+        var position = transform.position;
+        if (zCoord > 0) {
+            if (zCoordDelta > 0) {
+                zCoordDelta -= gravity;
+            }
+            else zCoordDelta -= gravity * 2;
+
+            zCoord += zCoordDelta;
+            position += Vector3.up * zCoordDelta;
+        }
+
+        if (zCoord <= 0) {
+            position -= Vector3.up*zCoord;
+            zCoord = 0;
+            zCoordDelta = 0;
+        }
+        //Jump stuff end ####
+        
+        position += speed * Input.GetAxis("Horizontal") * Vector3.right +
+                             speed * Input.GetAxis("Vertical") * Vector3.up;// + Vector3.up * zCoord;
+        transform.position = position;
+
+    }
+
+    private void Jump() {
+        zCoordDelta = startJumpHeight;
+        transform.position += Vector3.up * zCoordDelta;
+        zCoord += zCoordDelta;
+
+        lastJump = Time.time;
     }
 }
