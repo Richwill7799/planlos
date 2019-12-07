216ac848b57bd4ea38ab2d413dc487f7710a9840
Mini <minshan.luong@tum.de>
2019-12-07 01:42:42 +0100

    added health bar/slider; start game from menu screen

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Quit()
+    {
+       
+        Application.Quit();
+        
+    }
+}
