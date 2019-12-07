be223f94e05c7f1152b8cc8102583f3031067823
Simon Dittrich <sidit77@gmail.com>
2019-12-07 05:19:06 +0100

    bfs

---
+++ Assets/Materials/IceShader.shader
@@ -0,0 +1,66 @@
+﻿Shader "Custom/IceShader"
+{
+    Properties
+    {
+        _nColor ("Normal Color", Color) = (1,1,1,1)
+        _wColor ("Water Color", Color) = (1,1,1,1)
+        _eColor ("Edge Color", Color) = (1,1,1,1)
+        [PerRendererData] _CrackTex ("Cracks", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { 
+			"Queue"="Transparent" 
+			"IgnoreProjector"="True" 
+			"RenderType"="Transparent" 
+			"PreviewType"="Plane"
+			"CanUseSpriteAtlas"="True"
+		}
+
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		Blend One OneMinusSrcAlpha
+
+		Pass {
+		CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+			
+			struct appdata_t {
+				float4 vertex   : POSITION;
+				float2 texcoord : TEXCOORD0;
+			};
+
+			struct v2f {
+				float4 vertex   : SV_POSITION;
+				float2 texcoord  : TEXCOORD0;
+			};
+			
+			v2f vert(appdata_t IN) {
+				v2f OUT;
+				OUT.vertex = UnityObjectToClipPos(IN.vertex);
+				OUT.texcoord = IN.texcoord;
+
+				return OUT;
+			}
+
+			sampler2D _CrackTex;
+			float4 _nColor;
+			fixed4 _wColor;
+			fixed4 _eColor;
+
+			fixed4 frag(v2f IN) : SV_Target {
+			    fixed4 c = _wColor;
+			    if(tex2D(_CrackTex, IN.texcoord + float2(0.01, -0.05)).a > 0.1){
+					c = _eColor;
+				}
+				
+				c = lerp(_nColor, c, tex2D(_CrackTex, IN.texcoord).a);
+				return c;
+			}
+		ENDCG
+		}
+    }
+}
--- Assets/Scripts/IcePainter.cs
+++ Assets/Scripts/IcePainter.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class IcePainter : MonoBehaviour
 {
-    private const int size = 100;
+    private const int size = 500;
     
     
     public GameObject player;
@@ -12,11 +14,12 @@ public class IcePainter : MonoBehaviour
     private Texture2D texture;
     private (int, int) oldpos;
     private byte[] pixels;
+    private bool lastcomplete = true;
 
     void Start()
     {
-        texture = new Texture2D(size, size, TextureFormat.R8, false);
-        texture.filterMode = FilterMode.Point;
+        texture = new Texture2D(size, size, TextureFormat.Alpha8, false);
+        texture.filterMode = FilterMode.Bilinear;
         pixels = new byte[size * size];
         for (int i = 0; i < size; i++)
         {
@@ -27,10 +30,11 @@ public class IcePainter : MonoBehaviour
         }
         texture.SetPixelData(pixels, 0);
         texture.Apply();
-        
-        material = gameObject.GetComponent<MeshRenderer>().material;
-        material.mainTexture = texture;
 
+        MaterialPropertyBlock b = new MaterialPropertyBlock();
+        b.SetTexture(CrackTex, texture);
+        GetComponent<MeshRenderer>().SetPropertyBlock(b);
+        
         oldpos = GetPixelCoord();
     }
 
@@ -41,54 +45,91 @@ public class IcePainter : MonoBehaviour
     {
         (int, int) pc = GetPixelCoord();
 
-        int dx = pc.Item1 - oldpos.Item1;
-        int dy = pc.Item2 - oldpos.Item2;
+        int dx = Math.Sign(pc.Item1 - oldpos.Item1);
+        int dy = Math.Sign(pc.Item2 - oldpos.Item2);
 
         if (dx != 0 || dy != 0)
         {
-            if(dx * dx > 1 || dy * dy > 1)
-                Debug.Log("Fucking jumps");
-            int i = Mathf.Clamp(pc.Item1, 0, size) + Mathf.Clamp(pc.Item2, 0, size) * size;
-            if (pixels[i] != 0)
+            bool flood = false;
+
+            int nx = oldpos.Item1 + dx;
+            int ny = oldpos.Item2 + dy;
+
+            while (dx != 0 || dy != 0)
             {
+                int i = Mathf.Clamp(nx, 0, size) + 
+                        Mathf.Clamp(ny, 0, size) * size;
+                     
+                flood |= pixels[i] > 0 && lastcomplete; 
+                lastcomplete = pixels[i] == 0;
+                pixels[i] = 255;
                 
+                dx = Math.Sign(pc.Item1 - nx);
+                dy = Math.Sign(pc.Item2 - ny);
+
+                nx += dx;
+                ny += dy;
+            }
+
+           
+            
+            if (flood)
+            {
                 Debug.Log("Start flood");
                 Array.Clear(temp, 0, temp.Length);
-                if (fill(size / 2, size / 2))
+                fillqueue.Clear();
+                for (int x = 0; x < size; x++)
                 {
-                    for (int j = 0; j < size * size; j++)
+                    for (int y = 0; y < size; y++)
                     {
-                        if (temp[j])
-                            pixels[j] = 255;
+                        if (x == 0 || y == 0 || x == size - 1 || y == size - 1)
+                        {
+                            fillqueue.Enqueue((x, y));
+                        }
                     }
                 }
+
+                while (fillqueue.Count > 0)
+                {
+                    (int, int) c = fillqueue.Dequeue();
+                    int x = c.Item1;
+                    int y = c.Item2;
+                    if (x < 0 || y < 0 || x >= size || y >= size)
+                        continue;
+                    if (temp[x + y * size] || pixels[x + y * size] == 255) 
+                        continue;
+        
+                    //Debug.Log($"aiin {x} | {y}");
+                    temp[x + y * size] = true;
+
+                    fillqueue.Enqueue((x, y + 1));
+                    fillqueue.Enqueue((x + 1, y)); 
+                    fillqueue.Enqueue((x, y - 1)); 
+                    fillqueue.Enqueue((x - 1, y));                   
+                }
+                
+                for (int j = 0; j < size * size; j++)
+                {
+                    if (!temp[j])
+                        pixels[j] = Math.Max(pixels[j], (byte)50);
+                }
             }
-            pixels[i] = 255;
-            texture.SetPixelData(pixels, 0);
-            texture.Apply();
+            
         }
+        
+        for (int j = 0; j < size * size; j++)
+        {
+            if (pixels[j] > 10 && pixels[j] < 250)
+                pixels[j] = (byte)Math.Min(pixels[j] + 1, 250);
+        }
+        
+        texture.SetPixelData(pixels, 0);
+        texture.Apply();
+        
         oldpos = pc;
     }
     
     private bool[] temp = new bool[size * size];
-
-    private bool fill(int x, int y)
-    {
-        
-        //Debug.Log($"Testin {x} | {y}");
-        if (x < 0 || y < 0 || x >= size || y >= size)
-            return false;
-        if (temp[x + y * size] || pixels[x + y * size] != 0) 
-            return true;
-        
-        //Debug.Log($"aiin {x} | {y}");
-        temp[x + y * size] = true;
-
-        return  fill(x, y + 1) &&
-                fill(x, y - 1) && 
-                fill(x - 1, y) && 
-                fill(x + 1, y);
-    }
-    
-    
+    private Queue<(int, int)> fillqueue = new Queue<(int, int)>(size * 4 * 5);
+    private static readonly int CrackTex = Shader.PropertyToID("_CrackTex");
 }
