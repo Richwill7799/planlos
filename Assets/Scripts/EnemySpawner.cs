daf3cfbeea3e4a5c2c98572ff7355fd026dce9cc
Mini <minshan.luong@tum.de>
2019-12-08 00:02:35 +0100

    circular spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,9 +5,11 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject penguin;
     public float spawnRate = 1f;
+    public int selectSpawner;
 
     float lastSpawn;
-
+    float spawnRadius = 12f;
+    
 
     // Start is called before the first frame update
     void Start() {
@@ -18,10 +20,27 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        System.Random rnd = new System.Random();
+        //int selectSpawner = rnd.Next(0,2);
+        Vector3 spawnPos;
+        
 
         if (Time.time - lastSpawn >= spawnRate+Random.Range(0,7)) {
-            lastSpawn = Time.time;
-            Instantiate(penguin, transform.position, Quaternion.identity);
+            if(selectSpawner == 0)
+            {
+                lastSpawn = Time.time;
+                spawnPos = transform.position;
+            }
+            else
+            {
+                lastSpawn = Time.time;
+                float a = Random.Range(0f, 1f);
+                float b = Mathf.Sqrt(1 - Mathf.Pow(a, 2)) * rnd.Next(0, 2) * 2 - 1;
+                a *= rnd.Next(0, 2) * 2 - 1;
+
+                spawnPos = new Vector3(a * spawnRadius, b * spawnRadius, transform.position.z);
+            }
+            Instantiate(penguin, spawnPos, Quaternion.identity);
         }
     }
 }
