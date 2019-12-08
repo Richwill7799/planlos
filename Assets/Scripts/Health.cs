c06b1d81eac8283a5ad289ea954cc2d0b720d7b3
Mini <minshan.luong@tum.de>
2019-12-08 10:06:17 +0100

    swapped slider with health bar (image)

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -8,31 +8,37 @@ public class Health : MonoBehaviour
 {
     //public static Health instance { get; private set; }
     
-    public UnityEngine.UIElements.Slider slider2;
+    //public UnityEngine.UIElements.Slider slider2;
 
-    private float val = 1.0f;
+    Image healthBar;
 
-    private float sadjbl = 1.0f;
+    //private float val = 1.0f;
+
+    //private float sadjbl = 1.0f;
+
     // Start is called before the first frame update
     void Start() {
-        slider2.value = 1f;
+        //slider2.value = 1f;
         //val = 1f;
         //slider2 = GetComponent<Slider>();
+        healthBar = GetComponent<Image>();
     }
 
     // Update is called once per frame
     public void LateUpdate() {
         //slider2.value = sadjbl;
-        Debug.Log("Val: "+val + " and "+ sadjbl);
+        //Debug.Log("Val: "+val + " and "+ sadjbl);
+        
     }
 
     public void SetValue(float valuestogive) {
+        healthBar.fillAmount = valuestogive - 0.07f;
 
-        Debug.Log("Value given: " + valuestogive);
+        //Debug.Log("Value given: " + valuestogive);
         //slider2.value = value;
-        val = valuestogive;
-        sadjbl = valuestogive;
-        Debug.Log("Val: "+val);
+        //val = valuestogive;
+        //sadjbl = valuestogive;
+        //Debug.Log("Val: "+val);
 
         //slider.value = Mathf.MoveTowards(slider.value, value, 10f);
         //Debug.Log("update health value: " + slider.value);
